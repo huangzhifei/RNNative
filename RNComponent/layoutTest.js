faf8b09c05d8d292cce9de595c30bfc6d05d4dcb
huangzhifei <huangzhifei@jrq.com>
2017-09-18 17:49:50 +0800

    完善layout demo

--- RNComponent/layoutTest.js
+++ RNComponent/layoutTest.js
@@ -10,74 +10,124 @@ import {
 export default class LayoutTest extends Component {
     render() {
         return (
-            <Text style={{backgroundColor: '#aa88FF', marginTop: 74, flex: 0}}>
-            Hello !!!!!!!!
-            </Text>
+            <View style={style.parent}>
+
+                <View style={style.leftCol}>
+                    <View style={[style.cellOne, style.base]}>
+                        <Text style={{fontSize: 28, fontWeight: "bold", textAlign: 'center'}}>
+                            1
+                        </Text>
+                    </View>
+                    <View style={[style.cellTwo, style.base]}>
+                        <Text style={{fontSize: 28, fontWeight: "bold"}}>
+                            2
+                        </Text>
+                    </View>
+                    <View style={[style.cellThree, style.base]}>
+                        <Text style={{fontSize: 28, fontWeight: "bold"}}>
+                            3
+                        </Text>
+                    </View>
+                </View>
+
+                <View style={[style.rightCol]}>
+                    <View style={[style.cellFour,style.base]}>
+                        <Text style={{fontSize: 28, fontWeight: "bold"}}>
+                            4
+                        </Text>
+                    </View>
+                    <View style={[style.rightBottom]}>
+                        <View style={[style.cellFive, style.base]}>
+                            <Text style={{fontSize: 28, fontWeight: "bold"}}>
+                                5
+                            </Text>
+                        </View>
+                        <View style={[{flex: 1}, {flexDirection: 'column'}]}>
+                            <View style={[style.cellSix, style.base]}>
+                                <Text style={{fontSize: 28, fontWeight: "bold"}}>
+                                    6
+                                </Text>
+                            </View>
+                            <View style={[style.cellSeven, style.base]}>
+                                <Text style={{fontSize: 28, fontWeight: "bold"}}>
+                                    7
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+
+            </View>
         );
     }
 }
 
 const style = StyleSheet.create({
     parent: {
-        flexDirection: 'column',
+        flexDirection: 'row',
         position: 'absolute',
-        top: 64
+        top: 64,
+        left: 10,
+        right: 10,
+        bottom: 10
     },
 
     base: {
         borderColor: '#000000',
-        borderWidth: 4
-    },
-
-    topBox: {
-        flexDirection: 'row',
-        flex: 5
+        borderWidth: 4,
+        justifyContent: 'center', 
+        alignItems:'center'
     },
 
     leftCol: {
-        flex: 2
+        flex: 1,
+        flexDirection: 'column'
     },
 
-    bottomBox: {
-        flex: 2,
-        flexDirection: 'row'
-    },
-
-    bottomRight: {
+    rightCol: {
         flex: 2,
         flexDirection: 'column'
     },
 
+    rightBottom: {
+        flex: 2,
+        flexDirection: 'row'
+    },
+
     cellOne: {
         flex: 1,
-        borderBottomWidth: 15
+        borderBottomWidth: 12,
+        backgroundColor: '#FF0000'
     },
 
     cellTwo: {
-        flex: 3
+        flex: 3,
+        backgroundColor: '#00FF00',
     },
 
     cellThree: {
-        backgroundColor: '#FF0000',
-        flex: 5
+        backgroundColor: '#0000FF',
+        flex: 2
     },
 
     cellFour: {
-        flex: 3,
-        backgroundColor: '#0000FF'
+        backgroundColor: '#9999ff',
+        flex: 4,
     },
 
     cellFive: {
-        flex: 6
+        flex: 3,
+        backgroundColor: '#6600FF',
     },
 
     cellSix: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#5FB03D'
     },
 
     cellSeven: {
         flex: 1,
-        backgroundColor: '#FFFF00'
+        backgroundColor: '#EADE2C'
     }
 });
 
